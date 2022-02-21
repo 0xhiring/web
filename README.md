6ac9546140f30eaf3189d0c4fad36ee905d5e6fd
Nam Dam <namqdam@gmail.com>
2022-02-21 11:32:08 +0700

    Initialize

---
+++ README.md
@@ -0,0 +1,1 @@
+# 0xhiring web
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import 'rsuite/dist/rsuite.min.css';
+import './public/index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
